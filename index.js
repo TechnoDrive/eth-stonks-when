e7061a2b0bd1303deaa6040c6a4ff5b895557259
TheRealWendys <zarifabrar02@gmail.com>
2020-09-23 12:38:41 -0400

    First Commit

---
+++ index.js
@@ -0,0 +1,64 @@
+console.clear();
+
+const prompt = require('prompt-sync')({
+    input: process.stdin,
+    output: process.stdout
+});
+const {
+    Kraken
+} = require('node-crypto-api');
+
+const kraken = new Kraken();
+const notifier = require('node-notifier');
+const chalk = require('chalk');
+
+let targetPrice = Number(prompt(`${chalk.bold.white('What base price would you like to be alerted on (in USD)?')} ${chalk.bold.gray('(350, 200, 500, etc.)')} `));
+let checkerInterval = prompt(`${chalk.bold.white('Time between intervals to check ETH\'s price ')} ${chalk.bold.gray('(500 milliseconds, 5 seconds, 1 minute, 2 hours, etc.)')} `);
+// let exit = Boolean(prompt(`${chalk.bold.white('Would you like to repeat notifications?')} ${chalk.bold.gray('(More than notification after ETH hits the desired price, true or false, case sensitive)')} `))
+
+let timeConversionTable = {
+    plural: {
+        hours: 60 * 60 * 1000,
+        minutes: 60 * 1000,
+        seconds: 1000,
+        milliseconds: 1,
+    },
+    singular: {
+        hour: 60 * 60 * 1000,
+        minute: 60 * 1000,
+        second: 1000,
+        millisecond: 1
+    }
+}
+
+let time = checkerInterval.split(' ');
+time[1] = timeConversionTable.plural[time[1]] || timeConversionTable.singular[time[1]];
+time = time[0] * time[1];
+
+if (Number.isNaN(time)) {
+    console.log(chalk.red.bold('ERROR: Please enter a valid unit of time (hours, minutes, seconds, milliseconds)!'));
+    process.exit(0)
+}
+
+setInterval(() => {
+    kraken.ticker('ETH', 'USD')
+        .then(result => {
+            let currentPrice = result.result.XETHZUSD.o
+
+            if (Math.round(currentPrice) > targetPrice - 1) {
+                notifier.notify({
+                        title: `EthStonksWhen`,
+                        message: `ETH has reached ${targetPrice}!`,
+                        sound: true, // Only Notification Center or Windows Toasters
+                        wait: true, // Wait with callback, until user action is taken against notification, does not apply to Windows Toasters as they always wait or notify-send as it does not support the wait option
+                        timeout: 10,
+                        appID: 'Created by Hextanium#5890!'
+                    },
+                    function (err, response) {
+                        err ? console.log(err) : true;
+                    }
+                );
+            }
+        })
+        .catch(console.error);
+}, time)
---
+++ readmes/readme.txt
@@ -0,0 +1,21 @@
+eth-stonks-when
+
+What is EthStonksWhen?
+EthStonksWhen is a command line application designed to alert the user when ETH (a cryptocurrency) reaches a certain value.
+
+How do I use EthStonksWhen?
+After installing Node.js, and unzipping the file, run the following command in a windows shell:
+npm i chalk node-notifier node-crypto-api prompt-sync && launch
+
+How do I install Node.js?
+1. Go to the Node.js Website at https://nodejs.org/ 
+2. Click the "14.12.0" download button.
+3. Run the installer. 
+
+NOTE: Make sure to click "Add to PATH" during the installation, so that you can use the command globally.
+
+Known Bugs
+It is known that EthStonksWhen will continuously send notifications more than once.
+This may be super annoying for some users, so DM me (Hextanium#5890) to recieve a copy the non-spam-notifications.
+
+Made with love by Hextanium#5890 for the Salad community! (https://discord.gg/salad)
